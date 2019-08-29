98ad2ba3d813b399294492596058e067512d712b
Mariano Cortesi <mariano@celo.org>
2019-08-29 15:32:07 -0300

    [celo] Use @celo/ganache-core

--- lib.js
+++ lib.js
@@ -1,6 +1,6 @@
 // make sourcemaps work!
 require('source-map-support').install();
 
-module.exports = require("ganache-core/public-exports.js");
-module.exports.version = require("ganache-core/package.json").version;
-module.exports.to = require("ganache-core/lib/utils/to");
+module.exports = require("@celo/ganache-core/public-exports.js");
+module.exports.version = require("@celo/ganache-core/package.json").version;
+module.exports.to = require("@celo/ganache-core/lib/utils/to");
